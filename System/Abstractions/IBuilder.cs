05a8ea68bb803f4872190d3e5ec145c146609887
SleepWellPupper <SleepWellPupper@gmail.com>
2022-12-28 15:28:44 +0100

    Added builder, factory interfaces

---
+++ System/Abstractions/IBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace RhoMicro.Common.System.Abstractions
+{
+	public interface IBuilder<T>
+	{
+		T Build();
+		void Reset();
+	}
+}
---
+++ System/Abstractions/IFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhoMicro.Common.System.Abstractions
+{
+	public interface IFactory<T>
+	{
+		T Create();
+	}
+}
