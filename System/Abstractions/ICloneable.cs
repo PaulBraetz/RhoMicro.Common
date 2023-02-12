85057bb8b03b8c7cc19be8e4e726478978bd7e99
Paul Brätz <paul.braetz@rhomicro.com>
2023-02-12 18:53:02 +0100

    added ICloneable interface

---
+++ System/Abstractions/ICloneable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RhoMicro.Common.System.Abstractions
+{
+	/// <summary>
+	/// Supports cloning, which creates a new instance of a class with the same value
+	/// as an existing instance.
+	/// </summary>
+	/// <typeparam name="TSelf">This type.</typeparam>
+	public interface ICloneable<TSelf> : ICloneable
+	{
+		/// <summary>
+		/// Creates a new object that is a copy of the current instance.
+		/// </summary>
+		/// <returns>A new object that is a copy of this instance.</returns>
+		new TSelf Clone();
+	}
+}
