fe5c27f2990831918471901fe7af757a3b592a68
Paul Brätz <paul.braetz@rhomicro.com>
2023-03-02 16:38:15 +0100

    ReleaseCount is optional

--- System/Threading/Extensions.cs
+++ System/Threading/Extensions.cs
@@ -17,7 +17,7 @@ namespace RhoMicro.Common.System.Threading
 		/// <param name="gate">The semaphore for which to acquire a new release handle.</param>
 		/// <param name="releaseCount">The release count of the release handle created.</param>
 		/// <returns>A new instance of <see cref="SemaphoreReleaseHandle"/>, initialized to the semaphore and release count provided.</returns>
-		public static SemaphoreReleaseHandle GetReleaseHandle(this SemaphoreSlim gate, Int32 releaseCount)
+		public static SemaphoreReleaseHandle GetReleaseHandle(this SemaphoreSlim gate, Int32 releaseCount = 1)
 		{
 			gate.ThrowIfNull(nameof(gate));
 			releaseCount.ThrowIfNot(c => c > 0, $"{nameof(releaseCount)} must > 0.", nameof(releaseCount));
